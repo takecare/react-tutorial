410748dbe58b1a365aa5b70aa8b93fb8433c9f8d
takecare <rui@vazteixeira.org>
2018-04-15 18:30:55 +0100

    game class in own file

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Board } from './board.js';
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
+import { Game } from './game.js'
 
 ReactDOM.render(
   <Game />,
